1ee3a80bacda2a55b15fb7557f821c93e58bd284
JagdPietr <59313824+JagdPietr@users.noreply.github.com>
2020-08-10 09:55:00 +0800

    Drakon60 - Encoder Switch Correction - Changed the Position of the encoder switches to reflect actual positions - Correction of encoder switch on mock-up keymap - Transfer of OLED stuff (from keymap.c to keyboard.c) to fix OLED issue (not functioning through qmk config) - Addition of weak attribute to note that enum is from keymap.c

--- keyboards/drakon/rules.mk
+++ keyboards/drakon/rules.mk
@@ -20,7 +20,6 @@ BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 BLUETOOTH_ENABLE = no       # Enable Bluetooth
 AUDIO_ENABLE = no           # Audio output
-
 LTO_ENABLE = yes
 WPM_ENABLE = yes
 OLED_DRIVER_ENABLE = yes
